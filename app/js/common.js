59885eef0085534d126e31ff05a42850ab021ea5
Vasyl Alieksashyn <vasyl_alieksashyn@epam.com>
2017-05-25 12:42:53 +0300

    add carousel for services

--- app/js/common.js
+++ app/js/common.js
@@ -28,4 +28,54 @@ $(function() {
       $icon.removeClass("is-active");
     }, 100);
   });
+
+  $('.carousel-services').on('initialized.owl.carousel', function() {
+    setTimeout(function() {
+      carouselService();
+    }, 100);
+  });
+  $('.carousel-services').owlCarousel({
+    loop: true,
+    nav: true,
+    smartSpeed: 700,
+    navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+    responsiveClass: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      800: {
+        items: 2
+      },
+      1200: {
+        items: 3
+      }
+    }
+  });
+
+  $('.carousel-services-content').equalHeights();
+
+  function carouselService() {
+    $('.carousel-services-item').each(function() {
+      var $ths = $(this);
+      var $thsh = $ths.find('.carousel-services-content').outerHeight();
+      $ths.find('.carousel-services-image').css('min-height', $thsh);
+    });
+  };
+  carouselService();
+
+  $('.carousel-services-composition .h3').each(function() {
+    var $ths = $(this);
+    $ths.html($ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+  });
+
+  function onResize() {
+    $('.carousel-services-content').equalHeights();
+  };
+  onResize();
+  window.onresize = function() {
+    onResize();
+  };
+
 });
